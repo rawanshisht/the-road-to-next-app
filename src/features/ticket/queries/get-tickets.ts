662170aded20f3213cc2cc5fd488ddb29167e166
Rawan El-Shishtawy <36961921+rawanshisht@users.noreply.github.com>
2025-12-25 00:13:40 +0000

    try react error oundary and adding not found fallback route

--- src/features/ticket/queries/get-tickets.ts
+++ src/features/ticket/queries/get-tickets.ts
@@ -4,7 +4,6 @@ import { Ticket } from "../types";
 
 export const getTickets = async (): Promise<Ticket[]> => {
     await new Promise((resolve) => setTimeout(resolve, 2000));
-
     return new Promise((resolve) => {
         resolve(initialTickets);
     });
