61584c2615511256717ab532885143f128221c8a
Rawan El-Shishtawy <36961921+rawanshisht@users.noreply.github.com>
2025-12-26 22:17:57 +0000

    read data from database

--- src/features/ticket/queries/get-ticket.ts
+++ src/features/ticket/queries/get-ticket.ts
@@ -1,12 +1,10 @@
-import { initialTickets } from "@/data";
+import prisma from "@/lib/prisma"
 
-import { Ticket } from "../types";
 
-export const getTicket = async (ticketId: string): Promise<Ticket | null> => {
-    await new Promise((resolve) => setTimeout(resolve, 2000));
-
-    const maybeTicket = initialTickets.find((t) => t.id === ticketId);
-    return new Promise((resolve) => {
-        resolve(maybeTicket || null);
-    })
+export const getTicket = async (ticketId: string) => {
+    return await prisma.ticket.findUnique({
+        where: {
+            id: ticketId,
+        },
+    });
 }
