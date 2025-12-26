87ec6ff96c0ee808c391afdbc690ab9ce6c254b2
Rawan El-Shishtawy <36961921+rawanshisht@users.noreply.github.com>
2025-12-26 22:21:33 +0000

    use orm generated types

--- src/features/ticket/components/ticket-item.tsx
+++ src/features/ticket/components/ticket-item.tsx
@@ -4,10 +4,10 @@ import Link from "next/link";
 
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Ticket } from "@/generated/prisma/client";
 import { ticketPath } from "@/paths";
 
 import { TICKET_ICONS } from "../constants";
-import { Ticket } from "../types";
 
 type TicketItemProps = {
     ticket: Ticket,
--- src/features/ticket/types.ts
+++
@@ -1,7 +0,0 @@
-export type TicketStatus = 'OPEN' | 'IN_PROGRESS' | 'DONE';
-export type Ticket = {
-    id: string;
-    title: string;
-    description: string;
-    status: TicketStatus;
-}
